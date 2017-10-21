567da76400c1544793836896e47eca37c6b1f235
Unobtainiumrock <unobtainiumrock@gmail.com>
2017-10-21 15:56:00 -0500

    Fleshed out the file structure more with a clean, clear, and concise module structure

---
+++ src/components/video_list_detail.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const VideoListDetail =  () => {
+    return 4;
+}
+
+export default VideoListDetail;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
+//API KEY
 import API_KEY from '../API_KEY';
+
+// App Components
+import { SearchBar, VideoDetail, VideoListDetail, VideoList } from './components';
 
 const App = () => {
     return <div>Hello World!</div>;
