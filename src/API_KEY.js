b9d4347cf82259347a33734ae360e1b67f91f7df
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-02-25 11:46:58 -0800

    Add YouTube API_KEY

---
+++ src/API_KEY.js
@@ -0,0 +1,4 @@
+
+const API_KEY = 'AIzaSyDFISHvsHm8kcVbnOfaX390spMU0nktLIY';
+
+export default API_KEY;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-//API KEY
-import API_KEY from '../API_KEY';
+// API KEY
+// This will only work with your own API key, in a file at the speficied directory path
+// More information on setting up a YouTube API key can be found at https://console.developers.google.com
+import API_KEY from './API_KEY';
 
 // App Components
 import { SearchBar, VideoDetail, VideoListDetail, VideoList } from './components/index';
